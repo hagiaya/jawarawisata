3e48d400f979b42c80ad5e9abfd25ddb9c827c28
reza latandrang <123560246+hagiaya@users.noreply.github.com>
2026-03-01 00:38:34 +0800

    Add fallback supabase url and key to prevent deploy error

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
     return createBrowserClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+        process.env.NEXT_PUBLIC_SUPABASE_URL || 'http://localhost:54321',
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4aWFwd3BveWhtcnVhbWZweXZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkzMTUxMjksImV4cCI6MjAyNDg5MTEyOX0'
     )
 }
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -5,8 +5,8 @@ export async function createClient() {
     const cookieStore = await cookies()
 
     return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        process.env.NEXT_PUBLIC_SUPABASE_URL || 'http://localhost:54321',
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4aWFwd3BveWhtcnVhbWZweXZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkzMTUxMjksImV4cCI6MjAyNDg5MTEyOX0',
         {
             cookies: {
                 getAll() {
